6b81913cdf5a6ff14d6b1a0a971a72ae96bb2bfc
area1110 <97070827+nguyenkhanhhuyhe153585@users.noreply.github.com>
2022-02-06 18:21:38 +0700

    Fix with Bootstrap 4.0
    Fix calendar cannot display January

--- web/asset/script/customer/customer_dashboard.js
+++ web/asset/script/customer/customer_dashboard.js
@@ -41,8 +41,8 @@ generateCalendar = (month, year) => {
     calendar_days.innerHTML = ''
 
     let currDate = new Date()
-    if (!month) month = currDate.getMonth()
-    if (!year) year = currDate.getFullYear()
+    if (month == null || month == undefined) month = currDate.getMonth()
+    if (year == null || year == undefined) year = currDate.getFullYear()
 
     let curr_month = `${month_names[month]}`
     month_picker.innerHTML = curr_month
