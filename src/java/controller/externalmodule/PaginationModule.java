80940e6aa97b2b608f8c323cc3a5546618cfbca6
area1110 <97070827+nguyenkhanhhuyhe153585@users.noreply.github.com>
2022-02-11 12:11:03 +0700

    Update PaginationModule.java

--- src/java/controller/externalmodule/PaginationModule.java
+++ src/java/controller/externalmodule/PaginationModule.java
@@ -10,10 +10,12 @@ package controller.externalmodule;
  * @author area1
  */
 public class PaginationModule {
-//     public static int PAGE_SIZE = 2;
+    //public static int PAGE_SIZE = 2;
     // Đầu vào là Index của trang đó và số lượng bản ghi trên từng trang
     // trả về kết quả là 1 mảng 2 phần tử: phần tử đầu tiên là thứ tự bản ghi đầu tiên của 1 trang, phần tử thứ 2 là index 
     //  của bản ghi cuối cùng của 1 trang
+    // Khi query thì dùng câu lệnh "WHERE Row_count BETWEEN ? AND ?" và truyền 2 phần tử của mảng fromTo vào tương ứng
+    //Hàm này có thể dùng hoặc không dùng tùy ý
     
     public static int[] calcFromToRecord(int pageIndex, int PAGE_SIZE){
         int[] fromTo = new int[2];
