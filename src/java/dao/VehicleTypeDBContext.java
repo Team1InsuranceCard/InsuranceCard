02e98fb1a7db60cc9503f2a352fc590241167a32
Nguyen Manh Quy <quynmhe153283@fpt.edu.vn>
2022-02-17 10:41:16 +0700

    query to get vehicle type

--- src/java/dao/VehicleTypeDBContext.java
+++ src/java/dao/VehicleTypeDBContext.java
@@ -19,5 +19,23 @@ import model.VehicleType;
  */
 public class VehicleTypeDBContext extends DBContext {
 
-    
+    public ArrayList<VehicleType> getVehicleTypes() {
+        ArrayList<VehicleType> types = new ArrayList<>();
+        try {
+            String sql = "SELECT [ID]\n"
+                    + "      ,[VehicleType]\n"
+                    + "  FROM [VehicleType]";
+            PreparedStatement psm = connection.prepareStatement(sql);
+            ResultSet rs = psm.executeQuery();
+
+            while (rs.next()) {
+                VehicleType type = new VehicleType(rs.getInt("ID"), rs.getString("VehicleType"));
+                types.add(type);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return types;
+    }
 }
