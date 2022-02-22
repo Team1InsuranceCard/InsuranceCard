051d2dbe7b3afcff96de84259a2f9476079afa79
tuyethg2001 <92559118+tuyetntahe153175@users.noreply.github.com>
2022-02-22 17:09:14 +0700

    update staff edit customer
    join date readonly

--- src/java/dao/CustomerDBContext.java
+++ src/java/dao/CustomerDBContext.java
@@ -568,7 +568,6 @@ public class CustomerDBContext extends DBContext {
                     + "	, LastName = ?\n"
                     + "	, Address = ?\n"
                     + "	, Dob = ?\n"
-                    + "	, JoinDate = ?\n"
                     + "	, Phone = ?\n"
                     + "	, PersonalID = ?\n"
                     + "	, Province = ?\n"
