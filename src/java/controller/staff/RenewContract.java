9702ae8e0e37bc4934560e88a828b81a47ba70ad
tuyethg2001 <92559118+tuyetntahe153175@users.noreply.github.com>
2022-03-02 17:20:24 +0700

    BE staff renew contract update
    add delivery

--- src/java/controller/staff/RenewContract.java
+++ src/java/controller/staff/RenewContract.java
@@ -6,6 +6,7 @@
 package controller.staff;
 
 import dao.ContractDBContext;
+import dao.DeliveryDBContext;
 import java.io.IOException;
 import java.sql.Timestamp;
 import javax.servlet.ServletException;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Account;
 import model.Contract;
+import model.Delivery;
 import model.Staff;
 
 /**
@@ -21,7 +23,7 @@ import model.Staff;
  * @author DELL
  */
 public class RenewContract extends HttpServlet {
-    
+
     private Timestamp datetimeLocalToTimestamp(String datetimeLocal) {
         return Timestamp.valueOf(datetimeLocal.replace("T", " ") + ":00");
     }
@@ -38,11 +40,15 @@ public class RenewContract extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
-        ContractDBContext db = new ContractDBContext();
-        Contract contract = db.staffGetContractDetail(id);
-        
+        ContractDBContext conDB = new ContractDBContext();
+        Contract contract = conDB.staffGetContractDetail(id);
+
+        DeliveryDBContext delDB = new DeliveryDBContext();
+        Delivery delivery = delDB.getDeliveryByContract(id);
+
         request.setAttribute("contract", contract);
-        request.setAttribute("check", db.staffRenewCheck(contract));
+        request.setAttribute("check", conDB.staffRenewCheck(contract));
+        request.setAttribute("delivery", delivery);
         request.getRequestDispatcher("../../view/staff/renew_contract.jsp").forward(request, response);
     }
 
@@ -58,29 +64,49 @@ public class RenewContract extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
-        ContractDBContext db = new ContractDBContext();
-        Contract contract = db.staffGetContractDetail(id);
-        
+        ContractDBContext conDB = new ContractDBContext();
+        Contract contract = conDB.staffGetContractDetail(id);
+
         Timestamp newStartDate = datetimeLocalToTimestamp(request.getParameter("newStartDate"));
         Timestamp newEndDate = datetimeLocalToTimestamp(request.getParameter("newEndDate"));
         double newFee = Double.parseDouble(request.getParameter("newFee"));
         int payMethodID = Integer.parseInt(request.getParameter("payMethodID"));
-        
+        String fullName = request.getParameter("fullName");
+        String province = request.getParameter("province");
+        String phone = request.getParameter("phone");
+        String district = request.getParameter("district");
+        String email = request.getParameter("email");
+        String address = request.getParameter("address");
+
         Account staffAcc = (Account) request.getSession().getAttribute("account");
-        
+
         Staff staff = new Staff();
         staff.setAccount(staffAcc);
-        
+
         contract.setStartDate(newStartDate);
         contract.setEndDate(newEndDate);
         contract.setContractFee(newFee);
         contract.setStartStaff(staff);
+
+        int renewContractID = conDB.staffRenewContract(contract, payMethodID);
+        contract.setId(renewContractID);
         
-        int renewContractID = db.staffRenewContract(contract, payMethodID);
+        Delivery delivery = new Delivery();
+        delivery.setFullName(fullName);
+        delivery.setProvince(province);
+        delivery.setPhone(phone);
+        delivery.setDistrict(district);
+        delivery.setEmail(email);
+        delivery.setAddress(address);
+        delivery.setContract(contract);
         
+        DeliveryDBContext delDB = new DeliveryDBContext();
+        delDB.insertDelivery(delivery);
+
         request.setAttribute("contract", contract);
         request.setAttribute("isSuccess", true);
         request.setAttribute("renewContractID", renewContractID);
+        request.setAttribute("delivery", delivery);
         request.getRequestDispatcher("../../view/staff/renew_contract.jsp").forward(request, response);
     }
 
