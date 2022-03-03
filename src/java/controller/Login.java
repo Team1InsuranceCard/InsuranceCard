cbf6ec0b7b2fb1a30499c09243b555fdb4c4e6b3
NguyenQueTien <tiennqhe153125@fpt.edu.vn>
2022-03-03 22:12:32 +0700

    Update Login.java

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -6,6 +6,7 @@
 package controller;
 
 import dao.AccountDBContext;
+import dao.CustomerDBContext;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Account;
+import model.CustomerStaff;
 
 /**
  *
