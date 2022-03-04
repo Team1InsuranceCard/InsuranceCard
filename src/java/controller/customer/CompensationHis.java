d1a61f218bb6a4860e6ea78ab24c66f3d052e145
tuyethg2001 <92559118+tuyetntahe153175@users.noreply.github.com>
2022-03-04 12:28:04 +0700

    BE cus compen his

---
+++ src/java/controller/customer/CompensationHis.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.customer;
+
+import dao.CompensationDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Account;
+import model.Compensation;
+
+/**
+ *
+ * @author DELL
+ */
+public class CompensationHis extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Account acc = (Account) request.getSession().getAttribute("account");
+        
+        String raw_page = request.getParameter("page");
+        if (raw_page == null || raw_page.length() == 0) {
+            raw_page = "1";
+        }
+
+        int page = Integer.parseInt(raw_page);
+        int pageSize = 7;
+        
+        CompensationDBContext db = new CompensationDBContext();
+        ArrayList<Compensation> compensations = db.searchCompensationHis(acc.getId(), pageSize, page);
+        
+        int count = db.getTotalCustomerCompensation(acc.getId());
+        int totalPage = (count % pageSize == 0) ? count / pageSize : (count / pageSize) + 1;
+        
+        request.setAttribute("compensations", compensations);
+        request.setAttribute("totalPage", totalPage);
+        request.setAttribute("pageIndex", page);
+        request.getRequestDispatcher("../../view/customer/compensation_his.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
