e547bd32a089e03fe4706ddeb1190e826c28c8fd
tuyethg2001 <92559118+tuyetntahe153175@users.noreply.github.com>
2022-03-05 21:19:21 +0700

    BE cus accident his

--- src/java/dao/AccidentDBContext.java
+++ src/java/dao/AccidentDBContext.java
@@ -6,10 +6,12 @@
 package dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Accident;
+import model.Contract;
 
 /**
  *
@@ -30,7 +32,7 @@ public class AccidentDBContext extends DBContext {
                 + "           (? ,? ,? ,? ,? ,?,?)";
         try {
             PreparedStatement psm_insert_accident = connection.prepareStatement(sql_insert_accident);
-            int i=0;
+            int i = 0;
             psm_insert_accident.setTimestamp(++i, accident.getAccidentDate());
             psm_insert_accident.setString(++i, accident.getTitle());
             psm_insert_accident.setTimestamp(++i, accident.getCreatedDate());
@@ -38,10 +40,45 @@ public class AccidentDBContext extends DBContext {
             psm_insert_accident.setString(++i, accident.getHumanDamage());
             psm_insert_accident.setString(++i, accident.getVehicleDamage());
             psm_insert_accident.setInt(++i, accident.getContract().getId());
-            
+
             psm_insert_accident.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(AccidentDBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public Accident getAccident(int id) {
+        Accident accident = new Accident();
+        try {
+            String sql = "select Title\n"
+                    + "	, ContractID\n"
+                    + "	, CreatedDate\n"
+                    + "	, AccidentDate\n"
+                    + "	, HumanDamage\n"
+                    + "	, VehicleDamage\n"
+                    + "	, Attachment\n"
+                    + "from Accident\n"
+                    + "where ID = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Contract contract = new Contract();
+                contract.setId(rs.getInt("ContractID"));
+                
+                accident.setId(id);
+                accident.setTitle(rs.getString("Title"));
+                accident.setContract(contract);
+                accident.setCreatedDate(rs.getTimestamp("CreatedDate"));
+                accident.setAccidentDate(rs.getTimestamp("AccidentDate"));
+                accident.setHumanDamage(rs.getString("HumanDamage"));
+                accident.setVehicleDamage(rs.getString("VehicleDamage"));
+                accident.setAttatchment(rs.getString("Attachment"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccidentDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return accident;
+    }
+
 }
