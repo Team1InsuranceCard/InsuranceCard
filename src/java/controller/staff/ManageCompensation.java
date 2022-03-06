588c3cb1ac34233543ae3ebc8707357e933c9e74
area1110 <97070827+nguyenkhanhhuyhe153585@users.noreply.github.com>
2022-03-06 13:13:43 +0700

    Update ManageCompensation.java

--- src/java/controller/staff/ManageCompensation.java
+++ src/java/controller/staff/ManageCompensation.java
@@ -53,7 +53,7 @@ public class ManageCompensation extends HttpServlet {
         int pageIndex = Integer.parseInt(raw_pageIndex);
         HashMap<Integer, Compensation> compensations = compensationDBC
                 .getCompensationsByStaff(18, query, queryChoose, pageIndex, compensationStatus, orderby, ordertype);
-        int totalRecord = compensationDBC.totalCompensationsByStaff(18, query, queryChoose, compensationStatus);
+        int totalRecord = compensationDBC.totalCompensationsByStaff(account.getId(), query, queryChoose, compensationStatus);
         
         int totalPage = PaginationModule.calcTotalPage(totalRecord, 20);
         ArrayList<CompensationStatusCode> statusCode = statusCodeDBC.getCompensationStatusCodes();
