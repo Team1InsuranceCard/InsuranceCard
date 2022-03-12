5702fce49f7f36eb1412fd5f5750901bc05896d0
Nguyen Manh Quy <quynmhe153283@fpt.edu.vn>
2022-03-12 15:49:26 +0700

    BE checkout

--- src/java/controller/customer/CheckoutController.java
+++ src/java/controller/customer/CheckoutController.java
@@ -21,32 +21,6 @@ import model.Contract;
  */
 public class CheckoutController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CheckoutController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CheckoutController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -68,8 +42,8 @@ public class CheckoutController extends HttpServlet {
         Account account = (Account) request.getSession().getAttribute("account");
         
         ContractDBContext cdb = new ContractDBContext();
-//        Contract contract = cdb.getContractDetailByCustomer(account.getId(), Integer.parseInt(contractID));
-        Contract contract = cdb.getContractDetailByCustomer(4, Integer.parseInt(contractID));
+        Contract contract = cdb.getContractDetailByCustomer(account.getId(), Integer.parseInt(contractID));
+        //Contract contract = cdb.getContractDetailByCustomer(4, Integer.parseInt(contractID));
         
         request.setAttribute("contract", contract);
         request.getRequestDispatcher("../view/customer/checkout.jsp").forward(request, response);
@@ -86,7 +60,7 @@ public class CheckoutController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
